1940f21bdb51369c52c86f03fd3622811140f195
Sebastian Wieland <git-commits@nwie.land>
2021-03-31 21:39:37 +0200

    added netlify build script

---
+++ bin/build-netlify.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+npm run build
+
+cd dist/docs
+
+cat > index.html << EOF
+<!DOCTYPE html>
+<html>
+<head><title>Slide list</title></head>
+<body style="font-family: monospace">
+<ul>
+EOF
+
+for file in $(find . -name '*.slides.html' | sort); do
+  echo '<li><a href="'${file:2}'">'${file:2}'</a></li>' >> index.html
+done
+
+cat >> index.html << EOF
+</ul>
+</body>
+</html>
+EOF
