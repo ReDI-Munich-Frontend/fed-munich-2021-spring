b68f2b397b4bf49e30482ce6f8014bc64fa87554
Sebastian Wieland <sebasti@nwie.land>
2021-05-25 21:31:08 +0200

    Slides for lesson 15 (#16)
    * Slides for lesson 15
    
    * Minor adjustments for example
    
    * Added colour grid example
    
    * Added konami code example

---
+++ src/lessons/15_event_listeners/class-resources/hotel.js
@@ -0,0 +1,32 @@
+const hotelBookingForm = document.querySelector('#hotel-booking-form');
+const now = new Date();
+
+hotelBookingForm.arrival.min = now.toISOString().substring(0, 10);
+hotelBookingForm.departure.min = now.toISOString().substring(0, 10);
+
+checkDepartureValidity();
+hotelBookingForm.arrival.addEventListener('blur', () => {
+  checkDepartureValidity();
+});
+hotelBookingForm.departure.addEventListener('blur', () => {
+  checkDepartureValidity();
+});
+
+hotelBookingForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
+
+function checkDepartureValidity() {
+  if (!hotelBookingForm.departure.value || !hotelBookingForm.arrival.value) {
+    return;
+  }
+
+  const departureDate = new Date(hotelBookingForm.departure.value);
+  const arrivalDate = new Date(hotelBookingForm.arrival.value);
+
+  if (arrivalDate >= departureDate) {
+    hotelBookingForm.departure.setCustomValidity('The departure date must be later than the arrival date.');
+  } else {
+    hotelBookingForm.departure.setCustomValidity('');
+  }
+}
