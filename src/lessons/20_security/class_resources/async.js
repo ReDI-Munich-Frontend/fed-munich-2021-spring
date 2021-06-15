740dce5ec482d5315595407f2be5971874bc22f3
Marjorie Figueroa <marjoriefdev@gmail.com>
2021-06-15 14:32:23 +0200

    Lesson 20, recap on last lesson, a bit about the call stack and how JS works, security

---
+++ src/lessons/20_security/class_resources/async.js
@@ -0,0 +1,27 @@
+console.log('Hola!');
+console.log('Byee');
+
+
+// console.log('Hola!');
+// setTimeout(function() {
+//   console.log('Doing stuff');
+// }, 2000);
+// console.log('Byeee');
+
+// setInterval(function look() {
+//     console.log('Look!')
+// }, 100)
+
+// document.querySelector('#btn').addEventListener('click', function btnClick(e) {
+//     let n = 0
+//     while(n !== 1000000){
+
+//     }
+// })
+// document.querySelector("#btn").addEventListener("click", () => {
+//     let n = 0
+//     while(n !== 1000){
+//         console.log('Blocked')
+//         n++;
+//     }
+// });
